de8e73ee8922731ae9d10ecc418a53459fdaba90
wagnert <wagner_tim78@hotmail.com>
2017-04-22 14:34:47 +0200

    Add symfony DI/Bundle configuration

---
+++ symfony/ImportBundle.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * TechDivision\Import\Category\Ee\ImportBundle
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * PHP version 5
+ *
+ * @author    Tim Wagner <t.wagner@techdivision.com>
+ * @copyright 2016 TechDivision GmbH <info@techdivision.com>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      https://github.com/techdivision/import-category-ee
+ * @link      http://www.techdivision.com
+ */
+
+namespace TechDivision\Import\Category\Ee;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * The symfony bundle implementation for the M2IF import category EE library.
+ *
+ * @author    Tim Wagner <t.wagner@techdivision.com>
+ * @copyright 2016 TechDivision GmbH <info@techdivision.com>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      https://github.com/techdivision/import-category-ee
+ * @link      http://www.techdivision.com
+ */
+class ImportBundle extends Bundle
+{
+}
