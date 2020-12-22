46c92db6b316cc05dc25ca17a1e7e8d748b60dee
Martin Eisenführer <m.eisenfuehrer@techdivision.com>
2020-12-22 01:08:45 +0100

    PAC-307: blank url_key work with multi-store configuration

--- src/Repositories/SqlStatementRepository.php
+++ src/Repositories/SqlStatementRepository.php
@@ -241,7 +241,16 @@ class SqlStatementRepository extends \TechDivision\Import\Category\Repositories\
                     attribute_id = :attribute_id,
                     store_id = :store_id,
                     value = :value
-              WHERE value_id = :value_id'
+              WHERE value_id = :value_id',
+        SqlStatementKeys::CATEGORY_VARCHAR_BY_ATTRIBUTE_CODE_AND_ENTITY_TYPE_ID_AND_STORE_ID_AND_PK =>
+            'SELECT t1.*
+               FROM ${table:catalog_category_entity_varchar} t1,
+                    ${table:eav_attribute} t2
+              WHERE t2.attribute_code = :attribute_code
+                AND t2.entity_type_id = :entity_type_id
+                AND t1.attribute_id = t2.attribute_id
+                AND t1.store_id = :store_id
+                AND t1.row_id = :pk',
     );
 
     /**
