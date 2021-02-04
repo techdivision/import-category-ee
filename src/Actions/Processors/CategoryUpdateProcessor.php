cd8b9bde255c1517e2ae00873179ffbc5fad27b3
Tim Wagner <t.wagner@techdivision.com>
2021-02-04 17:34:49 +0100

    Add @deprecated annotations for upcoming 21.x branch

--- src/Actions/Processors/CategoryUpdateProcessor.php
+++ src/Actions/Processors/CategoryUpdateProcessor.php
@@ -25,11 +25,13 @@ use TechDivision\Import\Category\Ee\Utils\MemberNames;
 /**
  * The category update processor implementation.
  *
- * @author    Tim Wagner <t.wagner@techdivision.com>
- * @copyright 2016 TechDivision GmbH <info@techdivision.com>
- * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
- * @link      https://github.com/techdivision/import-category-ee
- * @link      http://www.techdivision.com
+ * @author     Tim Wagner <t.wagner@techdivision.com>
+ * @copyright  2021 TechDivision GmbH <info@techdivision.com>
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link       https://github.com/techdivision/import-category-ee
+ * @link       http://www.techdivision.com
+ * @deprecated Since 21.0.3
+ * @see        \TechDivision\Import\Actions\Processors\GenericIdentifierProcessor
  */
 class CategoryUpdateProcessor extends \TechDivision\Import\Category\Actions\Processors\CategoryUpdateProcessor
 {
