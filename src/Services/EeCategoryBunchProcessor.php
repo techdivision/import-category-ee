b1b8c78a258e340d44084caf833ae554ed840a0b
Martin Eisenführer <m.eisenfuehrer@techdivision.com>
2021-09-16 19:08:46 +0200

    Cleanup code and 3.8.x compatibility

--- src/Services/EeCategoryBunchProcessor.php
+++ src/Services/EeCategoryBunchProcessor.php
@@ -3,17 +3,11 @@
 /**
  * TechDivision\Import\Category\Ee\Services\EeCategoryBunchProcessor
  *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
- *
- * PHP version 5
+ * PHP version 7
  *
  * @author    Tim Wagner <t.wagner@techdivision.com>
  * @copyright 2019 TechDivision GmbH <info@techdivision.com>
- * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @license   https://opensource.org/licenses/MIT
  * @link      https://github.com/techdivision/import-category-ee
  * @link      http://www.techdivision.com
  */
@@ -43,7 +37,7 @@ use TechDivision\Import\Category\Ee\Actions\SequenceCategoryActionInterface;
  *
  * @author    Tim Wagner <t.wagner@techdivision.com>
  * @copyright 2019 TechDivision GmbH <info@techdivision.com>
- * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @license   https://opensource.org/licenses/MIT
  * @link      https://github.com/techdivision/import-category-ee
  * @link      http://www.techdivision.com
  */
